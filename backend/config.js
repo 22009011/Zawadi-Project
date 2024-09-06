41ea083250ffc062c5554a02c8eb2a76d67c1aee
Yatunyi Brian Ondeko <brianyondeko03>
2024-09-06 23:20:56 +0300

    changed databse to postgres from mysql during testing

--- backend/config.js
+++ backend/config.js
@@ -3,39 +3,43 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Create a Sequelize instance using MySQL
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  dialect: 'mysql', // Change dialect to 'mysql'
-  logging: console.log, // Enable logging (optional)
-});
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connected to the MySQL database.');
-  })
-  .catch(error => { 
-    console.error('Error connecting to the MySQL database:', error.message);
-  });
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: 'postgres',
-//   logging: console.log, // Enable logging
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false, // Adjust based on your SSL configuration
-//     },
-//   },
+// // Create a Sequelize instance using MySQL
+// const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+//   host: process.env.DATABASE_HOST,
+//   dialect: 'mysql', // Change dialect to 'mysql'
+//   logging: console.log, // Enable logging (optional)
 // });
 
 // sequelize.authenticate()
 //   .then(() => {
-//     console.log('Connected to the PostgreSQL database.');
+//     console.log('Connected to the MySQL database.');
 //   })
 //   .catch(error => { 
-//     console.error('Error connecting to the PostgreSQL database:', error.message);
+//     console.error('Error connecting to the MySQL database:', error.message);
 //   });
+
+
+
+
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: console.log, // Enable logging
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Adjust based on your SSL configuration
+    },
+  },
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to the PostgreSQL database.');
+  })
+  .catch(error => { 
+    console.error('Error connecting to the PostgreSQL database:', error.message);
+  });
 
 const jwtSecret = process.env.JWT_SECRET;
 
